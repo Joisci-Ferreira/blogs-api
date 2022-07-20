905a866ce7d0ed47367412f43813105986df36bc
Joiscilene <joisci.lene@hotmail.com>
2022-07-19 22:09:59 -0300

    Adicionando migrations e modelo User

---
+++ src/database/models/user.js
@@ -0,0 +1,20 @@
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, { timestamps: false });
+
+  User.associate = (models) => {
+    User.hasMany(models.User, { as: 'Users', foreignKey: 'id' });
+  }
+
+  return User
+}
+
+module.exports = User;
